267b077c53f82129a1fbb8ac5f7509566e465290
Sergiusz Kriwobokow <writesd@hotmail.com>
2018-09-06 12:51:27 +0200

    added: comments in bootstrap - sequence of calls

--- OMS/Bootstrapper.cs
+++ OMS/Bootstrapper.cs
@@ -22,6 +22,44 @@ namespace OMS
     {
         private MainWindowViewModel shellViewModel;
 
+        /// <summary>
+        /// 1 step of bootstrapping
+        /// </summary>
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(DashboardModule));
+            moduleCatalog.AddModule(typeof(OrdersModule));
+            moduleCatalog.AddModule(typeof(CustomersModule));
+            moduleCatalog.AddModule(typeof(EntitiesModule));
+            moduleCatalog.AddModule(typeof(EmployeesModule));
+            moduleCatalog.AddModule(typeof(ProductsModule));
+            moduleCatalog.AddModule(typeof(BannerModule));
+        }
+
+        /// <summary>
+        /// 2 step of bootstrapping
+        /// </summary>
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterType<IRegionNavigationContentLoader, ScopedRegionNavigationContentLoader>(
+                new ContainerControlledLifetimeManager());
+        }
+
+        /// <summary>
+        /// 3 step of bootstrapping
+        /// </summary>
+        protected override IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
+        {
+            var behaviors = base.ConfigureDefaultRegionBehaviors();
+            behaviors.AddIfMissing(RegionManagerAwareBehavior.BehaviorKey, typeof(RegionManagerAwareBehavior));
+            return behaviors;
+        }
+
+        /// <summary>
+        /// 4 step of bootstrapping
+        /// </summary>
         protected override DependencyObject CreateShell()
         {
             var mainWindow = Container.Resolve<MainWindow>();
@@ -30,37 +68,13 @@ namespace OMS
             return mainWindow;
         }
 
+        /// <summary>
+        /// 5 step of bootstrapping
+        /// </summary>
         protected override void InitializeShell()
         {
             shellViewModel.ConfigureRegionManager();
             Application.Current.MainWindow.Show();
-        }
-
-        protected override IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
-        {
-            var behaviors= base.ConfigureDefaultRegionBehaviors();
-            behaviors.AddIfMissing(RegionManagerAwareBehavior.BehaviorKey, typeof(RegionManagerAwareBehavior));
-            return behaviors;
-        }
-
-        protected override void ConfigureContainer()
-        {
-            base.ConfigureContainer();
-            Container.RegisterType<IRegionNavigationContentLoader, ScopedRegionNavigationContentLoader>(
-                new ContainerControlledLifetimeManager());
-        }
-        
-        protected override void ConfigureModuleCatalog()
-        {
-            var moduleCatalog = (ModuleCatalog)this.ModuleCatalog;        
-            moduleCatalog.AddModule(typeof(DashboardModule));
-            moduleCatalog.AddModule(typeof(OrdersModule));
-            moduleCatalog.AddModule(typeof(CustomersModule));
-            moduleCatalog.AddModule(typeof(EntitiesModule));
-            moduleCatalog.AddModule(typeof(EmployeesModule));
-            moduleCatalog.AddModule(typeof(ProductsModule));
-            moduleCatalog.AddModule(typeof(BannerModule));
-            
         }
     }
 }
