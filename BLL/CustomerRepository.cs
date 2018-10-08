19f9d6520d7965f31cf5a4ec3c8efcef25aa6bfd
Sergiusz Kriwobokow <writesd@hotmail.com>
2018-10-08 13:14:00 +0200

    added repositories

---
+++ BLL/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using DAL_LocalDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CustomerRepository : IGenericRepository<Customer>
+    {
+        LocalDbContext db;
+        public CustomerRepository(LocalDbContext context)
+        {
+            db = context;
+        }
+        public List<Customer> GetAll()
+        {
+            using (db)
+            {
+                var list = (from b in db.Customers
+                            select b).ToList();
+                return list;
+            }
+        }
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<List<Customer>> GetALlAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/OrderRepository.cs
@@ -0,0 +1,54 @@
+﻿using DAL_LocalDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class OrderRepository : IGenericRepository<Order>
+    {
+        LocalDbContext db;
+        public OrderRepository(LocalDbContext context)
+        {
+            db = context;
+        }
+
+        public List<Order> GetAll()
+        {
+            using (db)
+            {
+                var list = (from b in db.Orders
+                            select b).ToList();
+                return list;
+            }
+        }
+
+        public void Add(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<List<Order>> GetALlAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
