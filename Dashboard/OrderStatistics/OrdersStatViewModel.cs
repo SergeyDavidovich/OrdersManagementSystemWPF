a6cf27be46c2619899fd439a2eaa76e6da200c90
Sergiusz Kriwobokow <writesd@hotmail.com>
2018-10-14 11:54:15 +0200

    added query to Dashboard

--- Dashboard/OrderStatistics/OrdersStatViewModel.cs
+++ Dashboard/OrderStatistics/OrdersStatViewModel.cs
@@ -7,46 +7,97 @@ using Infrastructure.Base;
 using Prism;
 using DAL_LocalDb;
 using BLL;
+using Prism.Regions;
+using System.Data.Entity;
 
 namespace Dashboard.OrderStatistics
 {
-    public class OrdersStatViewModel : ViewModelBase
+    public class OrdersStatViewModel : NavigationAwareViewModelBase
     {
-        List<Order> orders;
-        List<Customer> customers;
-        public OrdersStatViewModel(IGenericRepository<Order> orderRepo, IGenericRepository<Customer> customerRepo)
+        LocalDbContext _context;
+
+        readonly List<Order> orders;
+        readonly List<Customer> customers;
+        readonly List<Order_Details> orderDetails;
+        readonly List<Category> categories;
+        readonly List<Product> products;
+
+        public OrdersStatViewModel(LocalDbContext context)
         {
             base.Title = "Orders";
+            _context = context;
 
-            //get data
-            orders = orderRepo.GetAll();
-            customers = customerRepo.GetAll();
+            //get data for all queries
+            _context.Orders.Load();
+            orders = _context.Orders.Local.ToList<Order>();
+
+            _context.Customers.Load();
+            customers = _context.Customers.ToList<Customer>();
+
+            _context.Order_Details.Load();
+            orderDetails = _context.Order_Details.ToList<Order_Details>();
+
+            _context.Categories.Load();
+            categories = _context.Categories.ToList<Category>();
+
+            _context.Products.Load();
+            products = _context.Products.ToList<Product>();
 
             //query for OrderByCountryGroups
-            var OrdersAndCountriess =
+            var OrdersAndCountries =
                 orders.Join(customers,
                 order => order.CustomerID,
                 cust => cust.CustomerID,
                (order, cust) => new { order.OrderID, cust.Country });
-            //projection on OrderByCountryGroups
-            this.OrderByCountryGroups = new List<OrderGroupsObject>(OrdersAndCountriess.GroupBy(c => c.Country).
-                Select(g => new OrderGroupsObject { Country = g.Key, Quantity = g.Count() }).OrderByDescending(g=>g.Quantity).Take(10));
-            //this.OrderGroupsMock = new List<OrderGroupsObject>()
-            //{
-            //    new OrderGroupsObject(){Country="USA", Quantity=10},
-            //    new OrderGroupsObject(){Country="GB", Quantity=20},
-            //    new OrderGroupsObject(){Country="Germany", Quantity=30},
-            //    new OrderGroupsObject(){Country="Poland", Quantity=15},
-            //    new OrderGroupsObject(){Country="Czech Republic", Quantity=25}
-            //};
+
+            //projection on OrderByCountryObject
+            this.OrderByCountryGroups =
+                new List<OrderByCountryObject>(OrdersAndCountries.GroupBy(c => c.Country).
+                Select(g => new OrderByCountryObject { Country = g.Key, Quantity = g.Count() }).
+                OrderByDescending(g => g.Quantity).
+                Take(10));
+
+            //SELECT
+            //dbo.Categories.CategoryName,
+            //SUM(dbo.[Order Details].UnitPrice) AS[Sum of Orders]
+            //FROM
+            //dbo.Categories INNER JOIN dbo.Products
+            //ON dbo.Categories.CategoryID = dbo.Products.CategoryID
+            //INNER JOIN dbo.[Order Details]
+            //ON dbo.Products.ProductID = dbo.[Order Details].ProductID
+            //GROUP BY dbo.Categories.CategoryName
+
+            //query for OrderByCategoryGroups
+            var OrdersAndCategories =
+                categories.Join(products,
+                cat => cat.CategoryID,
+                pro => pro.CategoryID,
+                (cat, pro) => new { cat.CategoryName, pro.ProductID }).
+                Join(orderDetails,
+                pro => pro.ProductID,
+                det => det.ProductID,
+                (pro, det) => new { name = pro.CategoryName, price = det.UnitPrice });
+
+            //projection on OrderByCategoryObject
+            this.OrderByCategoryGroups =
+            new List<OrderByCategoryObject>(OrdersAndCategories.GroupBy(c => c.name).
+            Select(g => new OrderByCategoryObject { CategoryName = g.Key, SumOfSale = g.Sum(c => c.price) }));
         }
-        public class OrderGroupsObject
+            
+            
+            public class OrderByCountryObject
         {
             public string Country { get; set; }
             public int Quantity { get; set; }
         }
-        public List<OrderGroupsObject> OrderGroupsMock { get; set; }
+        public class OrderByCategoryObject
+        {
+            public string CategoryName { get; set; }
+            public decimal SumOfSale { get; set; }
+        }
 
-        public List<OrderGroupsObject> OrderByCountryGroups { get; set; }
+        public List<OrderByCountryObject> OrderByCountryGroups { get; set; }
+        public List<OrderByCategoryObject> OrderByCategoryGroups { get; set; }
+
     }
 }
