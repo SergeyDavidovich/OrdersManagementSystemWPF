4af26b62986a04e69fcfd66948fd7b92d790b621
Sergiusz Kriwobokow <writesd@hotmail.com>
2018-10-15 11:12:20 +0200

    changed Dashboard widgets and syle

--- Dashboard/CustomerStatistics/CustomerStatsViewModel.cs
+++ Dashboard/CustomerStatistics/CustomerStatsViewModel.cs
@@ -8,19 +8,44 @@ using Infrastructure;
 using Infrastructure.Base;
 using Prism.Commands;
 using Prism.Regions;
+using DAL_LocalDb;
+using System.Data.Entity;
 
 namespace Dashboard.CustomerStatistics
 {
     public class CustomerStatsViewModel : ViewModelBase
     {
-        private readonly IRegionManager _regionManager;
+        LocalDbContext _context;
+        readonly List<Customer> customers;
 
-        public CustomerStatsViewModel(IRegionManager regionManager)
+        public CustomerStatsViewModel(LocalDbContext context)
         {
             Title = "Customers";
-        }
+            _context = context;
 
-        #region Commands
-        #endregion
+            _context.Customers.Load();
+            customers = _context.Customers.ToList<Customer>();
+
+
+            // SELECT top(10)
+            // COUNT(CustomerID), Country
+            // FROM[NORTHWIND].[dbo].[Customers]
+            // GROUP BY Country ORDER BY COUNT(CustomerID) DESC
+
+            //projection on CustomerByCountryObject
+            CustomerByCountryGroups = 
+                new List<CustomerByCountryObject>(customers.GroupBy(c => c.Country).
+                Select(g => new CustomerByCountryObject { Country = g.Key, Quantity = g.Count() }));
+        }
+        public class CustomerByCountryObject
+        {
+            public int Quantity { get; set; }
+            public string Country { get; set; }
+        }
+        public List<CustomerByCountryObject> CustomerByCountryGroups { get; set; }
+
+
+
+
     }
 }
