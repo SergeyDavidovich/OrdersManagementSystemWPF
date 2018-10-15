51c71d8f22fcefd834149e62f6288da553081f99
Sergiusz Kriwobokow <writesd@hotmail.com>
2018-10-15 18:28:01 +0200

    added data sourses to Dashboard Customers

--- Dashboard/CustomerStatistics/CustomerStatsViewModel.cs
+++ Dashboard/CustomerStatistics/CustomerStatsViewModel.cs
@@ -17,6 +17,9 @@ namespace Dashboard.CustomerStatistics
     {
         LocalDbContext _context;
         readonly List<Customer> customers;
+        readonly List<Order> orders;
+        readonly List<Order_Details> orderDetails;
+
 
         public CustomerStatsViewModel(LocalDbContext context)
         {
@@ -26,6 +29,11 @@ namespace Dashboard.CustomerStatistics
             _context.Customers.Load();
             customers = _context.Customers.ToList<Customer>();
 
+            _context.Orders.Load();
+            orders = _context.Orders.Local.ToList<Order>();
+
+            _context.Order_Details.Load();
+            orderDetails = _context.Order_Details.ToList<Order_Details>();
 
             // SELECT top(10)
             // COUNT(CustomerID), Country
@@ -33,9 +41,35 @@ namespace Dashboard.CustomerStatistics
             // GROUP BY Country ORDER BY COUNT(CustomerID) DESC
 
             //projection on CustomerByCountryObject
-            CustomerByCountryGroups = 
+            CustomerByCountryGroups =
                 new List<CustomerByCountryObject>(customers.GroupBy(c => c.Country).
                 Select(g => new CustomerByCountryObject { Country = g.Key, Quantity = g.Count() }));
+
+            //SELECT TOP 10
+            //Customers.CompanyName, SUM([Order Details].UnitPrice * [Order Details].Quantity) AS Purchaces
+            //FROM
+            //[Orders] INNER JOIN[Order Details] ON[Order Details].OrderID = Orders.OrderID
+            //INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+            //GROUP BY Customers.CompanyName
+            //order by Purchaces DESC
+
+            var CustomersAndOrders =
+                orders.Join(orderDetails,
+                or => or.OrderID,
+                det => det.OrderID,
+                (or, det) => new { or.CustomerID, det.UnitPrice, det.Quantity })
+                .Join(customers,
+                o => o.CustomerID,
+                cust => cust.CustomerID,
+                (or, cust) => new { name = cust.CompanyName, price = or.UnitPrice * or.Quantity });
+
+            //projection on PurchasingByCustomerGroups
+            this.PurchasingByCustomerGroups =
+                new List<PurchasingByCustomerObject>(CustomersAndOrders.GroupBy(c => c.name)
+                .Select(g=> new PurchasingByCustomerObject {Name=g.Key, SumPurchasing=g.Sum(c=>c.price) })
+                .OrderByDescending(p=>p.SumPurchasing)
+                .Take(10));
+
         }
         public class CustomerByCountryObject
         {
@@ -43,6 +77,13 @@ namespace Dashboard.CustomerStatistics
             public string Country { get; set; }
         }
         public List<CustomerByCountryObject> CustomerByCountryGroups { get; set; }
+
+        public class PurchasingByCustomerObject
+        {
+            public string Name { get; set; }
+            public decimal SumPurchasing { get; set; }
+        }
+        public List<PurchasingByCustomerObject> PurchasingByCustomerGroups { get; set; }
 
 
 
