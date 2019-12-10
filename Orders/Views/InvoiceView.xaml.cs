9c4cfbd49ec87ffd13ec88c5d8256297972771ee
Sergiusz Kriwobokow <writesd@hotmail.com>
2019-12-10 08:21:33 +0100

    small references optimisation

--- Orders/Views/InvoiceView.xaml.cs
+++ Orders/Views/InvoiceView.xaml.cs
@@ -1,24 +1,8 @@
-﻿using DAL_LocalDb;
-using Microsoft.Practices.Unity;
+﻿using Microsoft.Practices.Unity;
 using Orders.ViewModels;
-using Prism.Regions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Orders.Events;
 using Prism.Events;
-using Orders.ViewModels;
 using System.IO;
 using System.Diagnostics;
 
